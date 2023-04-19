d72d472dfde9ffcb1711f1c420b7f28446f33550
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-19 10:31:23 -0500

    Lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,7 +17,11 @@ const routes: Routes = [
     path: 'contact',
     component: ContactPageComponent,
   },
-
+  {
+    path: 'countries',
+    loadChildren: () =>
+      import('./countries/countries.module').then((m) => m.CountriesModule),
+  },
   {
     path: '**',
     redirectTo: '',
