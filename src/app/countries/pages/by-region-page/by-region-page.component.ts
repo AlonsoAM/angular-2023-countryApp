24eba246b0232eb99a9afa5529a512c05ead52ba
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-04-20 19:32:36 -0500

    Termiando buqueda por Pais y por Region

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country.interface';
 
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
   styles: [],
 })
-export class ByRegionPageComponent {}
+export class ByRegionPageComponent {
+  public regions: string[] = ['asia', 'america', 'africa', 'europe', 'oceania'];
+  public countries: Country[] = [];
+
+  constructor(private countriesService: CountriesService) {}
+
+  buscarPaisPorRegion(region: string): void {
+    this.countriesService
+      .searchRegion(region)
+      .subscribe((countries) => (this.countries = countries));
+  }
+}
--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -17,4 +17,18 @@ export class CountriesService {
       .get<Country[]>(url)
       .pipe(catchError((error) => of([]))); // Retorna un nuevo observable de un array vacío
   }
+
+  searchCountry(term: string): Observable<Country[]> {
+    const url: string = `${this._apiURl}/name/${term}`;
+    return this.httpClient
+      .get<Country[]>(url)
+      .pipe(catchError((error) => of([])));
+  }
+
+  searchRegion(region: string): Observable<Country[]> {
+    const url: string = `${this._apiURl}/region/${region}`;
+    return this.httpClient
+      .get<Country[]>(url)
+      .pipe(catchError((error) => of([])));
+  }
 }
